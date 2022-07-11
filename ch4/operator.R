d9e9f25ae45158fc4adc780cd5b83047a5a7817f
devuk <devuk0204@gmail.com>
2022-07-11 12:30:10 +0900

    operator

---
+++ ch4/operator.R
@@ -0,0 +1,58 @@
+#arithmetic operator
+num1 <- 100
+num2 <- 20
+result <- num1 + num2
+result
+
+result <- num1 - num2
+result
+
+result <- num1 * num2
+result
+
+result <- num1 / num2
+result
+
+result <- num1 %% num2
+result
+
+result <- num1 ^ num2
+result
+
+result <- num1 ^ 2
+result
+
+#Relation operator
+boolean <- num1 == num2
+boolean
+
+boolean <- num1 != num2
+boolean
+
+boolean <- num1 > num2
+boolean
+
+boolean <- num1 >= num2
+boolean
+
+boolean <- num1 < num2
+boolean
+
+boolean <- num1 <= num2
+boolean
+
+#Logical operator
+logical <- num1 >= 50 && num2 <= 10
+logical
+
+logical <- num1 >= 50 | num2 <= 10
+logical
+
+logical <- num1 >= 50
+logical
+
+logical <- !(num1 >= 50)
+logical
+
+x <- TRUE; y <- FALSE
+xor(x, y)
